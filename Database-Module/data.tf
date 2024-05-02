fb42fce5cb99377a647443c79c0d217ad64325d2
s5edouard <140545236+s5edouard@users.noreply.github.com>
2024-05-02 14:52:14 -0400

    Update data.tf

--- Database-Module/data.tf
+++ Database-Module/data.tf
@@ -1,18 +1,15 @@
 
-# Get secret information for RDS password
-data "aws_secretsmanager_secret" "rds_password" {
-  # name = lookup(var.postgres, "rds_password_secretsmanager_secret_path")
-  name = "bfa/devops"
-}
-data "aws_secretsmanager_secret_version" "rds_password" {
-  secret_id = data.aws_secretsmanager_secret.rds_password.id
+
+data "aws_secretsmanager_secret" "example" {
+  name = "olivia-secret02"
+  depends_on = [
+    aws_secretsmanager_secret.example
+  ]
 }
 
-# Get secret information for RDS username
-data "aws_secretsmanager_secret" "rds_username" {
-  # 
-  name = "bfa/devops"
-}
-data "aws_secretsmanager_secret_version" "rds_username" {
-  secret_id = data.aws_secretsmanager_secret.rds_username.id
+
+data "aws_secretsmanager_secret_version" "secret" {
+  # Specify the necessary parameters here
+  secret_id = data.aws_secretsmanager_secret.example.id
+  
 }
