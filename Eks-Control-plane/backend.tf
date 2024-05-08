ef4bd85572535e9d1446c443022010ed48e1a6db
s5edouard <140545236+s5edouard@users.noreply.github.com>
2024-05-08 16:17:38 -0400

    Create backend.tf

---
+++ Eks-Control-plane/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "2923-devops-bfa-final-tf-state"
+    key            = "aws-terraform/control-plane/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "2923-devops-bfa-final-tf-state-lock"
+  }
+}
