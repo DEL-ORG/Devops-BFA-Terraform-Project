52e69da48eac4c355bb2bf7b4751dafdd13deed9
s5edouard <140545236+s5edouard@users.noreply.github.com>
2024-05-12 14:04:13 -0400

    Update data-subnet-priv.tf

--- Eks-worker-node-module/data-subnet-priv.tf
+++ Eks-worker-node-module/data-subnet-priv.tf
@@ -1,20 +1,20 @@
 data "aws_subnet" "sub_priv1" {
   filter {
     name   = "tag:Name"
-    values = ["revive-1300-pri_subnet-1"]
+    values = ["2923-devops-bfa-final-private-01-us-east-2a"]
   }
 }
 
 data "aws_subnet" "sub_priv2" {
   filter {
     name   = "tag:Name"
-    values = ["revive-1300-pri_subnet-2"]
+    values = ["2923-devops-bfa-final-private-02-us-east-2b"]
   }
 }
 
 data "aws_subnet" "sub_priv3" {
   filter {
     name   = "tag:Name"
-    values = ["revive-1300-pri_subnet-3"]
+    values = ["2923-devops-bfa-final-private-03-us-east-2c"]
   }
 }
