fcf86c2ec18d4599c976ac728fca208f92cfe86e
s5edouard <140545236+s5edouard@users.noreply.github.com>
2024-05-24 14:32:49 -0400

    Update data-subnet-priv.tf

--- Eks-worker-node-module/data-subnet-priv.tf
+++ Eks-worker-node-module/data-subnet-priv.tf
@@ -1,20 +1,20 @@
 data "aws_subnet" "sub_priv1" {
   filter {
     name   = "tag:Name"
-    values = ["2923-devops-bfa-final-private-01-us-east-2a"]
+    values = ["2923-devops-bfa-final-private-01-us-east-1a"]
   }
 }
 
 data "aws_subnet" "sub_priv2" {
   filter {
     name   = "tag:Name"
-    values = ["2923-devops-bfa-final-private-02-us-east-2b"]
+    values = ["2923-devops-bfa-final-private-02-us-east-1b"]
   }
 }
 
 data "aws_subnet" "sub_priv3" {
   filter {
     name   = "tag:Name"
-    values = ["2923-devops-bfa-final-private-03-us-east-2c"]
+    values = ["2923-devops-bfa-final-private-03-us-east-1c"]
   }
 }
