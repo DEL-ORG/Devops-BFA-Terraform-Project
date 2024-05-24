597761125a7fc9d69161a7ce3c7095c3d7e27960
devopseasylearning <info@devopseasylearning.com>
2024-05-24 15:14:02 -0400

    changes

--- Namespace-Module/data.tf
+++ Namespace-Module/data.tf
@@ -1,11 +1,11 @@
-data "aws_eks_cluster" "revive" {
+data "aws_eks_cluster" "olivia-k8s" {
   
-  # name = "dev-revive"
+  # name = "olivia-k8s"
   name = var.control_plane_name
 }
 
-data "aws_eks_cluster_auth" "revive" {
+data "aws_eks_cluster_auth" "olivia-k8s" {
 
-  # name = "dev-revive"
+  # name = "olivia-k8s"
   name = var.control_plane_name
 }
