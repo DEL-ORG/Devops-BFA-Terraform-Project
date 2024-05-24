fc62f5794fac172df2ed606dcbf2be95815d2a22
s5edouard <140545236+s5edouard@users.noreply.github.com>
2024-05-24 14:38:56 -0400

    Update backend.tf

--- Database-Module/backend.tf
+++ Database-Module/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "2923-devops-bfa-final-tf-state"
     key            = "aws-terraform/Database/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     dynamodb_table = "2923-devops-bfa-final-tf-state-lock"
   }
 }
