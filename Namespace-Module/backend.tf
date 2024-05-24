440510d2a9df92bb0c3390cecabc0462a2aa2182
devopseasylearning <info@devopseasylearning.com>
2024-05-24 14:52:39 -0400

    changes

---
+++ Namespace-Module/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "2923-devops-bfa-final-tf-state"
+    key            = "aws-terraform/Namespace/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "2923-devops-bfa-final-tf-state-lock"
+  }
+}
