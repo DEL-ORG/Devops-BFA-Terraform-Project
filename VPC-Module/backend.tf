25945d20bcaf1074fb76b0571e283906980d150e
devopseasylearning <info@devopseasylearning.com>
2024-06-10 16:28:04 -0400

    changes

--- VPC-Module/backend.tf
+++ VPC-Module/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "2923-devops-bfa-final-tf-state"
+    bucket         = "2108-devops-bfa-final-tf-state"
     key            = "aws-terraform/vpc/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "2923-devops-bfa-final-tf-state-lock"
+    dynamodb_table = "2108-devops-bfa-final-tf-state-lock"
   }
 }
