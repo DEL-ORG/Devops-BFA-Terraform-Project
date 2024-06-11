241960553d8d1f6c10c49bf719731c230b3155e4
s5kisito <139710772+s5kisito@users.noreply.github.com>
2024-06-11 16:38:30 -0400

    Update backend.tf

--- Bastion-host-Modules/bastion-host-creation/backend.tf
+++ Bastion-host-Modules/bastion-host-creation/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "2108-devops-bfa-final-tf-state"
-    key            = "aws-terraform/vpc/terraform.tfstate"
+    key            = "aws-terraform/bastion-host/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "2108-devops-bfa-final-tf-state-lock"
   }
