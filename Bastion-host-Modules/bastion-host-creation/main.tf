25dc7093cb9262d564de398c49bef7e5b261a519
s5kisito <139710772+s5kisito@users.noreply.github.com>
2024-06-11 16:40:12 -0400

    Update main.tf

--- Bastion-host-Modules/bastion-host-creation/main.tf
+++ Bastion-host-Modules/bastion-host-creation/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "bastion_host" {
-  ami                    = "ami-0f15b007c8270e1f6"
+  ami                    = "ami-05f364fdb936b1c01"
   instance_type          = "t2.xlarge"
   subnet_id              = "subnet-025a8b87cffa50b8f"
-  key_name               = "olivia23"
+  key_name               = "chriskeypair"
   vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
 
   tags = {
@@ -10,11 +10,4 @@ resource "aws_instance" "bastion_host" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "2923-devops-bfa-final-tf-state"
-    key            = "aws-terraform/bastion-host/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "2923-devops-bfa-final-tf-state-lock"
-  }
-}
+
