48c471dcf8b0bb6ccb66aa9808303ad815262de9
s5kisito <139710772+s5kisito@users.noreply.github.com>
2024-06-13 15:17:43 -0400

    Update variable.tf

--- Eks-Control-plane/variable.tf
+++ Eks-Control-plane/variable.tf
@@ -1,19 +1,19 @@
-variable "tags" {
+ariable "tags" {
   type        = map(string)
   description = "Common tags to be applied to resources"
   default = {
-  "id"             = "2923"
-  "owner"          = "S5edouard"
-  "teams"          = "devops-BFA"
-  "environment"    = "devops-bfa"
-  "project"        = "final"
-  "create_by"      = "Terraform"
-  "cloud_provider" = "aws"
+    "id"             = "2108"
+    "owner"          = "S5KISITO"
+    "teams"          = "usa-devops"
+    "environment"    = "devops-bfa"
+    "project"        = "final"
+    "create_by"      = "Terraform"
+    "cloud_provider" = "aws"
   }
 }
 
 variable "region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
@@ -22,25 +22,26 @@ variable "cluster_name" {
 }
 
 variable "eks_version" {
-  type = string
+  type    = string
   default = "1.29"
 }
 
 variable "endpoint_private_access" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "endpoint_public_access" {
-  type = bool
+  type    = bool
   default = true
 }
 
 variable "public_subnets" {
   type = map(string)
   default = {
-    us-east-1a = "subnet-01ca715268155b78e"
-    us-east-1b = "subnet-0224a65c32f4a12b5"
-    us-east-1c = "subnet-02b0aa8c03ca80d14"
+
+    us-east-1a = "subnet-078e9659bf03f9e19"
+    us-east-1b = "subnet-08d58d81c9075242a"
+    us-east-1c = "subnet-0fb7c2f86f8af1028"
   }
 }
