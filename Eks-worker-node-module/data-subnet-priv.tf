d7a01d37b45ef9ce1631657054e9a088059a51e1
s5kisito <139710772+s5kisito@users.noreply.github.com>
2024-06-13 15:20:10 -0400

    Update data-subnet-priv.tf

--- Eks-worker-node-module/data-subnet-priv.tf
+++ Eks-worker-node-module/data-subnet-priv.tf
@@ -1,20 +1,20 @@
 data "aws_subnet" "sub_priv1" {
   filter {
     name   = "tag:Name"
-    values = ["2923-devops-bfa-final-private-01-us-east-1a"]
+    values = ["2108-devops-bfa-final-private-01-us-east-1a"]
   }
 }
 
 data "aws_subnet" "sub_priv2" {
   filter {
     name   = "tag:Name"
-    values = ["2923-devops-bfa-final-private-02-us-east-1b"]
+    values = ["2108-devops-bfa-final-private-02-us-east-1b"]
   }
 }
 
 data "aws_subnet" "sub_priv3" {
   filter {
     name   = "tag:Name"
-    values = ["2923-devops-bfa-final-private-03-us-east-1c"]
+    values = ["2108-devops-bfa-final-private-03-us-east-1c"]
   }
 }
