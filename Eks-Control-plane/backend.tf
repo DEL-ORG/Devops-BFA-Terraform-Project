56ce0c05d49688837882701cfa515025942707d4
s5kisito <139710772+s5kisito@users.noreply.github.com>
2024-06-13 15:18:15 -0400

    Update backend.tf

--- Eks-Control-plane/backend.tf
+++ Eks-Control-plane/backend.tf
@@ -1,8 +1,9 @@
+
 terraform {
   backend "s3" {
-    bucket         = "2923-devops-bfa-final-tf-state"
-    key            = "aws-terraform/control-plane/terraform.tfstate"
+    bucket         = "2108-devops-bfa-final-tf-state"
+    key            = "aws-terraform/Control-plane/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "2923-devops-bfa-final-tf-state-lock"
+    dynamodb_table = "2108-devops-bfa-final-tf-state-lock"
   }
 }
